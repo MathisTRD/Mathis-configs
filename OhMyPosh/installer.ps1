3855c868d02e229fb09ccab88bfb3ba6a103f1b9
Mathis <mathishecktr@gmail.com>
2024-04-03 17:28:36 +0200

    New OhMyPosh config

--- OhMyPosh/installer.ps1
+++ OhMyPosh/installer.ps1
@@ -1,11 +1,10 @@
 $Path = "$env:LOCALAPPDATA/Programs/oh-my-posh/"
-
 $mydocuments = [Environment]::GetFolderPath("MyDocuments")
 
-# Download OhMyPosh Theme JSON
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/MathisTRD/configs/main/OhMyPosh/theme/mathis.omp.json -OutFile "$Path/mathis.omp.json"
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+Invoke-Command { & "pwsh.exe"       } -NoNewScope
 
-# Add content to the downloaded JSON file
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/MathisTRD/configs/main/OhMyPosh/theme/mathis.omp.json -OutFile "$Path/mathis.omp.json"
 
 $mydocuments = [Environment]::GetFolderPath("MyDocuments")
 Add-Content -Path "$mydocuments/Powershell/Microsoft.PowerShell_profile.ps1" -Value "oh-my-posh init pwsh --config 'C:\Users\Mathis\AppData\Local\Programs\oh-my-posh\mathis.omp.json' | Invoke-Expression"
